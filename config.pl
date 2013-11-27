33b0305a1e619280d67b6a1a8f50470e265d2cfc
jkff90 <jkff90@yahoo.com>
2013-11-27 16:49:10 +0700

    Create config.pl

---
+++ config.pl
@@ -0,0 +1,13 @@
+%global_config = (
+   TOOL           => 'QUESTASIM',         # available options: QUESTASIM, NCVERILOG
+   METHODOLOGY    => 'UVM',               # available options: OVM, UVM
+   EXTENSIONS     => ['v', 'sv', 'vt'],
+   COVERAGE       => '',                   # QuestaSim: , NcVerilog: 
+   TAB            => 4
+);
+
+#%questasim_config = (
+#);
+#
+#%ncverilog_config = (
+#); 
